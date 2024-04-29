8c54ebc1cb8b009a50196e711cdf2ac9197e3294
Shariq Khan <shariqkhanns@gmail.com>
2024-04-30 01:45:39 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Navigation from './Components/Navigation.jsx'
+import HeroSection from './Components/Hero.jsx'
+
+function App() {
+  return (
+    <div>
+      <Navigation />
+      <HeroSection />
+    </div> 
+  )
+}
+
+export default App
